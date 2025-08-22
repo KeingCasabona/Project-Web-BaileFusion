1bc356561c9c3a6ce77083255c0b120a7e876918
KeingCasabona <keing.limache.casabona@gmail.com>
2025-08-22 16:54:11 -0500

    feat: Add Header-CreateDisc-Form

--- app/(routes)/teacher/components/Header/FormCreateDisc/FormCreateDisc.tsx
+++ app/(routes)/teacher/components/Header/FormCreateDisc/FormCreateDisc.tsx
@@ -1,7 +1,70 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import { formSchema } from "./FormCreateDisc.form"
+
+
 export function FormCreateDisc() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            discName: "",
+            slug: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
     return (
-        <div>
-            FormCreateDisc
-        </div>
-    );
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-4">
+                <FormField
+                    control={form.control}
+                    name="discName"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nombre del disco</FormLabel>
+                            <FormControl>
+                                <Input  {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="slug"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nombre para el enlace web</FormLabel>
+                            <FormControl>
+                                <Input  {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Crear disco</Button>
+            </form>
+        </Form>
+    )
 }
