21524485f754b2a9aad083c0f1c4a744c136e26c
KeingCasabona <keing.limache.casabona@gmail.com>
2025-08-26 11:56:08 -0500

    feat: Add onPublish

--- app/(routes)/teacher/[discId]/[musicId]/components/MusicForm/MusicForm.tsx
+++ app/(routes)/teacher/[discId]/[musicId]/components/MusicForm/MusicForm.tsx
@@ -4,6 +4,8 @@ import { MusicFormProps } from "./MusicForm.types";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { TitleBlock } from "../../../components";
+import axios from "axios";
+import { toast } from "sonner";
 
 
 
@@ -15,9 +17,35 @@ export function MusicForm(props: MusicFormProps) {
         return null;
     }
 
+    const onPublish = async (state: boolean) => {
+        try {
+            await axios.patch(`/api/disc/${discId}/music/${music.id}`, {
+                isPublished: state
+            });
+            if (state) {
+                toast("Música publicada", {
+                    style: { background: "#16a34a", color: "white" }, // verde
+                });
+            } else {
+                toast("Música NO publicada", {
+                    style: { background: "#facc15", color: "black" }, // amarillo
+                });
+            }
+
+
+            router.refresh();
+        } catch (error) {
+            console.log(error);
+            toast("Ocurrió un error", {
+                style: { background: "#dc2626", color: "white" }, // rojo
+            });
+        }
+    }
+
     return (
-        <div>
-            <div >
+
+        <div >
+            <div>
                 <Button
                     className="mb-2 hover:text-[#0D0C11] hover:bg-[#E9E6ED] cursor-pointer"
                     // variant="outline"
@@ -25,30 +53,29 @@ export function MusicForm(props: MusicFormProps) {
                     <ArrowLeft />
                     Volver a la edición del Disco
                 </Button>
-                <div className="p-6 mt-6 bg-[#1E182E] rounded-md flex justify-between items-center text-[#E9E6ED]">
-                    <TitleBlock title="Configuración de la música" icon={Settings} />
-                    <div className="gap-2 flex items-center">
-                        {music.isPublished ? (
-                            <Button
-                                className="bg-amber-400 text-[#0D0C11] border-0 hover:bg-[#E9E6ED] hover:text-[#0D0C11] cursor-pointer"
-                                onClick={() => console.log("Ocultar musica")}>
-                                Ocultar
-                            </Button>
-                        ) : (
-                            <Button
-                                className="bg-emerald-700 text-[#E9E6ED] border-0 hover:bg-[#E9E6ED] hover:text-[#0D0C11] cursor-pointer"
-                                onClick={() => console.log("Publicar musica")}>
-                                Mostrar
-                            </Button>
-                        )}
+            </div >
+            <div className="p-6 mt-6 bg-[#1E182E] rounded-md flex justify-between items-center text-[#E9E6ED]">
+                <TitleBlock title="Configuración de la música" icon={Settings} />
+                <div className="gap-2 flex items-center">
+                    {music.isPublished ? (
                         <Button
-                            className="border-0 hover:bg-[#E9E6ED] hover:text-[#0D0C11] cursor-pointer"
-                            variant="destructive"
-                            onClick={() => console.log("Eliminar musica")}>
-                            <Trash />
+                            className="bg-amber-400 text-[#0D0C11] border-0 hover:bg-[#E9E6ED] hover:text-[#0D0C11] cursor-pointer"
+                            onClick={() => onPublish(false)}>
+                            No Publicar
                         </Button>
-                    </div>
-
+                    ) : (
+                        <Button
+                            className="bg-emerald-700 text-[#E9E6ED] border-0 hover:bg-[#E9E6ED] hover:text-[#0D0C11] cursor-pointer"
+                            onClick={() => onPublish(true)}>
+                            Publicar
+                        </Button>
+                    )}
+                    <Button
+                        className="border-0 hover:bg-[#E9E6ED] hover:text-[#0D0C11] cursor-pointer"
+                        variant="destructive"
+                        onClick={() => console.log("Eliminar musica")}>
+                        <Trash />
+                    </Button>
                 </div>
             </div>
         </div>
---
+++ app/api/disc/[discId]/music/[musicId]/route.ts
@@ -0,0 +1,34 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { ca } from "zod/v4/locales";
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: Promise<{ discId: string, musicId: string }> }
+) {
+    try {
+        const { userId } = await auth();
+        const { discId, musicId } = await params;
+        const values = await req.json();
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        const music = await prisma.music.update({
+            where: {
+                id: musicId,
+                discId: discId,
+            },
+            data: {
+                ...values,
+            }
+        });
+
+        return NextResponse.json(music);
+    } catch (error) {
+        console.log("[DISC_MUSIC_UPDATE]", error);
+
+        return new NextResponse("Internal server error", { status: 500 });
+    }
+}
